b0a85dee10733cbcc7f9484e0eb562a22da3230b
Illia Dyrda <106555644+dyrdaillia@users.noreply.github.com>
2024-02-26 17:00:31 +0100

    Add files via upload

---
+++ LibraryController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace WebApplication4.Controllers
+{
+    public class LibraryController : Controller
+    {
+        private readonly IConfiguration _configuration; 
+        public LibraryController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return Content("Welcome to the Library!");
+        }
+
+        public async Task<IActionResult> Books()
+        {
+            var booksList = await System.IO.File.ReadAllTextAsync(_configuration["BooksFilePath"]);
+            return Content(booksList);
+        }
+
+        public async Task<IActionResult> Profile(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                id = "current";
+            }
+            
+            var userProfile = await System.IO.File.ReadAllTextAsync($"{_configuration["ProfilesDirectoryPath"]}/{id}.json");
+            return Content(userProfile);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace WebApplication4
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapGet("/Library", async context =>
+                {
+                    await context.Response.WriteAsync("Welcome to the Library!");
+                });
+
+                endpoints.MapGet("/Library/Books", async context =>
+                {
+                    var booksList = await File.ReadAllTextAsync(Configuration["BooksFilePath"]);
+                    await context.Response.WriteAsync(booksList);
+                });
+
+                endpoints.MapGet("/Library/Profile/{id?}", async context =>
+                {
+                    
+                    var id = context.Request.RouteValues["id"] as string;
+
+                    if (string.IsNullOrEmpty(id))
+                    {
+                        id = "current";
+                    }
+
+                    var userProfile = await File.ReadAllTextAsync($"{Configuration["ProfilesDirectoryPath"]}/{id}.json");
+                    await context.Response.WriteAsync(userProfile);
+                });
+            });
+        }
+    }
+}
